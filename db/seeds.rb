0bd3be2c4974eba2e811193f8babf7e422cdde05
Ethan Teh <tehzl@hotmail.com>
2022-07-29 16:53:02 +0800

    .

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+5.times do |_|
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone_in_e164,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+  restaurant.save
+end
+
+20.times do |i|
+  restaurant = Restaurant.find(i % 5 + 1)
+  review = Review.new(
+    rating: rand(0..5),
+    content: Faker::Lorem.sentence,
+    restaurant: restaurant
+  )
+  review.save
+end
